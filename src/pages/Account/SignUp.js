18b87f596f5028297362e8bb69ab518754e1168f
sobiyab <124256759+sobiyab@users.noreply.github.com>
2023-11-07 14:25:16 +0530

    Update SignUp.js

--- src/pages/Account/SignUp.js
+++ src/pages/Account/SignUp.js
@@ -83,7 +83,7 @@ const Signup = () => {
       alert('Please fill in all fields');
     } else {
       try {
-        const response = await fetch('/signup', {
+        const response = await fetch('https://backenddd-w6f5.onrender.com/signup', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
