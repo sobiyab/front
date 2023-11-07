e59261bd4fd66194c8290cadb61ca847ce7d8426
sobiyab <124256759+sobiyab@users.noreply.github.com>
2023-11-07 14:23:49 +0530

    Update SignIn.js

--- src/pages/Account/SignIn.js
+++ src/pages/Account/SignIn.js
@@ -17,7 +17,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch('/coachlog', {
+      const response = await fetch('https://backenddd-w6f5.onrender.com/coachlog', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
